11fd167262e678f6f5e1d0be5be0cadef73694f3
Marcelo Cavalini <m.cavalini@speedwell.com.au>
2021-06-09 13:38:53 +1000

    Began to insert few Solid principles

---
+++ 1-solid/0-SingleResponsibilityPrinciple.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_solid
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text) { 
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index) {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false) {
+            if (overwrite || !File.Exists(filename)) {
+                File.WriteAllText(filename, j.ToString());
+            }
+        }
+    }
+
+    //public class Demo
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var j = new Journal();
+    //        j.AddEntry("I saw Batman");
+    //        j.AddEntry("Superman can fly");
+
+    //        Console.WriteLine(j);
+
+    //        var p = new Persistence();
+    //        var filename = @"c:\temp\journal.txt";
+    //        p.SaveToFile(j, filename, true);
+
+    //        Process.Start("notepad.exe", filename);
+    //    }
+    //}
+}
---
+++ 1-solid/1-Open-Closed Principle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+
+// Opens to extention
+// Close to modification
+namespace _1_solid
+{
+    public enum Color { Red, Green, Blue }
+    public enum Size { Small, Medium, Large, Huge}
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            if(name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter {
+
+        #region It breaks the Open Close Principle
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size) {
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
+        }
+
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+
+        public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var p in products)
+                if (p.Size == size && p.Color == color)
+                    yield return p;
+        }
+        #endregion
+
+    }
+
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            WriteLine("Green products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green)) {
+                WriteLine($" - {p.Name} is green");
+            }
+
+        }
+    }
+}
